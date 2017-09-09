b98e4d8f75a7b2c04a2d1a0b0b073ea22dd8dda1
laijie <laijie1994@163.com>
2017-09-09 13:38:14 +0800

    log server

--- client/udpClient.php
+++ client/udpClient.php
@@ -10,7 +10,7 @@ namespace client;
  */
 $client = new \swoole_client(SWOOLE_SOCK_UDP);
 
-if (!$client->connect('127.0.0.1', 9501, -1))
+if (!$client->connect('127.0.0.1', 9502, -1))
 {
     exit("connect failed. Error: {$client->errCode}\n");
 }
