2e61dd7e75f336bd4542f49f90afcb547e71f0b8
laijie <laijie1994@163.com>
2017-10-14 12:14:13 +0800

    测试mysqli类插入数据

--- test.php
+++ test.php
@@ -13,9 +13,27 @@ $db = new Mysqlidb(Array(
     'charset' => null));
 if (!$db) die("Database error");
 
-var_dump($db);PHP_EOL;
+//var_dump($db);PHP_EOL;
 
 $db->setTrace(true);
+
+if (!$db->ping()) {
+    echo "db is not up";
+    exit;
+}
+
+$data = Array (
+    "id" => "",
+    "user_id" => 1,
+    "content" => 'test this mysql class can use or not',
+    "pid" => 1,
+    "post_id" => 1,
+    "created_at" => $db->now(),
+    "updated_at" => $db->now(),
+);
+$id = $db->insert ('comments', $data);
+if($id)
+    echo 'user was created. Id=' . $id;
 
 die;
 
