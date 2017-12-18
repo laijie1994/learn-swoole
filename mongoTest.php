e89c4303da027200a2e2d09fa075eec272f6c7a0
laijie <laijie1994@163.com>
2017-12-18 17:38:58 +0800

    update

--- mongoTest.php
+++ mongoTest.php
@@ -1,11 +1,11 @@
 <?php
 include "./ini.php";
 
-echo "<pre/>";
+//echo "<pre/>";
 $mongo = new mumongo('127.0.0.1:27017');
 
 //print_r($mongo);
 
-$mongo->selectDataBase("test","test");
+var_dump($mongo->selectDataBase("test","test"));
 
 var_dump($mongo->getOne(["name"=>"石老师"]));
