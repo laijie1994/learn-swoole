e528ed884d1783d1e3db21afc369d90bcc2aae78
jeremy.lai <jeremy.lai@huolala.cn>
2017-12-18 17:35:31 +0800

    mongo 操作

--- mongoTest.php
+++ mongoTest.php
@@ -4,4 +4,8 @@ include "./ini.php";
 echo "<pre/>";
 $mongo = new mumongo('127.0.0.1:27017');
 
-print_r($mongo);
+//print_r($mongo);
+
+$mongo->selectDataBase("test","test");
+
+var_dump($mongo->getOne(["name"=>"石老师"]));
